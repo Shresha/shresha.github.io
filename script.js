4e6161a35b07723dd5baa5d404b7fa9f89f027ab
sdas338 <sdas338@gatech.edu>
2023-11-26 15:40:46 -0500

    personal website

---
+++ script.js
@@ -0,0 +1,30 @@
+
+let stars = document.getElementById('stars');
+let moon = document.getElementById("moon");
+let mountains_behind = document.getElementById('mountains_behind');
+let text = document.getElementById('text');
+let btn = document.getElementById('btn');
+let mountains_front = document.getElementById('mountains_front');
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    stars.style.left = value * 0.25 + 'px';
+    moon.style.top = value * 1.05 + 'px';
+    mountains_behind.style.top = value * 0.5 + 'px';
+    mountains_front.style.top = value * 0 + 'px';
+    text.style.marginRight = value * 2 + 'px';
+    text.style.marginTop = value * 0.5 + 'px';
+    btn.style.marginTop = value * 1.5 + 'px';
+})
+
+//typing animation script
+window.addEventListener('load', function() {
+    var typed = new Typed("#typing", {
+      strings:["Web Developer", "Software Engineer", "Student", "Teaching Assistant"],
+      typeSpeed: 100,
+      backSpeed: 100,
+      loop: true
+    });
+  });
+
+
+  
